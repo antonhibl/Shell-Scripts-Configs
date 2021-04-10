dfb68fda15b7c8dbc15d74206d4258be5b855a69
antonhibl <75855379+antonhibl@users.noreply.github.com>
2021-04-10 11:35:33 -0700

    Custom Python Script
    won't work for other people without tweaks

---
+++ snakesake.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import sys, os.path
+if sys.argv[1] == '1': print(os.path.expanduser('~/Coding/python/fun'))
+elif sys.argv[1] == '2':  print(os.path.expanduser('~/Coding/python/school'))
