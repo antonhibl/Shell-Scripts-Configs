398ef8b292f5dcd3372f5693e33a156364964957
antonhibl <antonhibl11@gmail.com>
2021-11-24 21:52:33 -0700

    clearing for new configurations files

--- ansicompimproved.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd ~/coding/C/testing
-yourfilenames=`ls`
-for eachfile in $yourfilenames
-frin == 1
-do
-   echo $eachfile
-done
--- snakesake.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-import sys, os.path
-if sys.argv[1] == '1': print(os.path.expanduser('~/Coding/python/fun'))
-elif sys.argv[1] == '2':  print(os.path.expanduser('~/Coding/python/school'))
